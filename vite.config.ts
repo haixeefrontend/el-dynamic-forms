329eeb650f2595dad64ec4fa213e3e8a25e6a8dc
Maiko Tan <maiko.tan.coding@gmail.com>
2025-11-10 17:19:09 +0800

    chore: add element-plus / unocss and related packages and configurations
    Signed-off-by: Maiko Tan <maiko.tan.coding@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import dts from 'vite-plugin-dts'
+import unocss from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import ElementPlus from 'unplugin-element-plus/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 
 import packageJson from './package.json'
+import unoConfig from './uno.config'
 
 function getPackageName() {
   const { name } = packageJson
@@ -24,6 +30,25 @@ export default defineConfig({
         // https://publint.dev/rules#export_types_invalid_format
         copyFileSync('dist/index.d.ts', 'dist/index.d.cts')
       },
+    }),
+    unocss(unoConfig),
+    AutoImport({
+      imports: [
+        'vue',
+      ],
+      dts: true,
+      eslintrc: {
+        enabled: true,
+      },
+    }),
+    ElementPlus({ defaultLocale: 'zh-cn', useSource: true }),
+    Components({
+      dirs: [],
+      globs: [],
+      resolvers: [
+        ElementPlusResolver({ importStyle: 'sass' }),
+      ],
+      dts: true,
     }),
   ],
   build: {
